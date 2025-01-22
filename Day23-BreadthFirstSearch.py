a8d988b466aabca756cbb0153b9abfb4d2758524
nidaislam <121117931+NidaIslam@users.noreply.github.com>
2025-01-22 22:52:31 +0500

    Add Day23 code to find the BST level traversal

---
+++ Day23-BreadthFirstSearch.py
@@ -0,0 +1,36 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        #Write your code here
+        queue = [root]
+        for q in queue:
+            if q.left:
+                queue.append(q.left)
+            if q.right:
+                queue.append(q.right)
+        print(" ".join([str(i.data) for i in queue]))
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
